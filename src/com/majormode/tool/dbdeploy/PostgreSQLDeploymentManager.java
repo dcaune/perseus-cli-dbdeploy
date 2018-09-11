14a1fd391a8d9f1c0107316b958274b89600e157
Daniel CAUNE <daniel.caune@gmail.com>
2018-09-11 10:40:22 +0700

    Add debugging information.

--- src/com/majormode/tool/dbdeploy/PostgreSQLDeploymentManager.java
+++ src/com/majormode/tool/dbdeploy/PostgreSQLDeploymentManager.java
@@ -173,11 +173,12 @@ public class PostgreSQLDeploymentManager extends SQLDeploymentManager {
       Matcher runtimeParameterCommandMatcher = m_patternRuntimeParameterCommand.matcher(sqlExpression);
 
       if (runtimeParameterCommandMatcher.find()) {
+        System.out.println("Runtime parameter command: " + sqlExpression);
         String parameterName = matcher.group("name");
         String command = matcher.group(1).trim();
         runtimeParameterCommands.put(parameterName, command);
       } else if (sqlExpression.length() > 0) {
-        System.out.println(sqlExpression);
+        System.out.println("SQL statement: " + sqlExpression);
         statements.add(new SQLStatement(sqlExpression, sqlScript, new ArrayList(runtimeParameterCommands.values())));
       }
     }
